2908f5cf201b23ead2c37c5774b82c77eb136588
JivkoSp <atjspasov@gmail.com>
2025-08-03 15:36:34 +0300

    Added Vos/Vos/Controls/TextView.cs and Vos/Vos/Controls/TextViewDrawable.cs

---
+++ Vos/Vos/Controls/TextView.cs
@@ -0,0 +1,24 @@
+﻿namespace Vos.Controls
+{
+    public class TextView : ContentView
+    {
+        private GraphicsView _graphicsView;
+        
+        private TextViewDrawable _drawable;
+
+        public string Text { get; set; } = string.Empty;
+
+        public TextView()
+        {
+            _drawable = new TextViewDrawable(this);
+
+            _graphicsView = new GraphicsView
+            {
+                Drawable = _drawable,
+                BackgroundColor = Colors.White,
+            };
+
+            Content = _graphicsView;
+        }
+    }
+}
---
+++ Vos/Vos/Controls/TextViewDrawable.cs
@@ -0,0 +1,17 @@
+﻿namespace Vos.Controls
+{
+    public class TextViewDrawable : IDrawable
+    {
+        private readonly TextView _textView;
+
+        public TextViewDrawable(TextView textView)
+        {
+            _textView = textView;
+        }
+
+        public void Draw(ICanvas canvas, RectF dirtyRect)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
