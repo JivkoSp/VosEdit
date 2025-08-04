3c208c95c7b2d23463e93d28ab1e695db9bc4f09
JivkoSp <atjspasov@gmail.com>
2025-08-04 23:54:01 +0300

    Added Document extensions in Vos/Vos/Extensions/SegmentExtensions.cs

---
+++ Vos/Vos/Extensions/SegmentExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Vos.Interfaces.Models.Document;
+
+namespace Vos.Extensions
+{
+    public static class SegmentExtensions
+    {
+        public static bool Contains(this ISegment segment, int offset, int length)
+            => segment.Offset <= offset && offset + length <= segment.EndOffset;
+
+        public static bool Contains(this ISegment thisSegment, ISegment segment)
+            => segment != null && thisSegment.Offset <= segment.Offset && segment.EndOffset <= thisSegment.EndOffset;
+    }
+}
