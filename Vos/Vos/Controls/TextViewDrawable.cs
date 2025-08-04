81909f966caeb43f12df40b83557b471340ff4d1
JivkoSp <atjspasov@gmail.com>
2025-08-04 23:57:16 +0300

    Updated Vos/Vos/Controls/TextViewDrawable.cs

--- Vos/Vos/Controls/TextViewDrawable.cs
+++ Vos/Vos/Controls/TextViewDrawable.cs
@@ -11,7 +11,44 @@
 
         public void Draw(ICanvas canvas, RectF dirtyRect)
         {
-            throw new NotImplementedException();
+            canvas.FillColor = Colors.White;
+
+            canvas.FillRectangle(dirtyRect);
+
+            if (string.IsNullOrEmpty(_textView.Text))
+            {
+                return;
+            }
+
+            // Draw text line by line
+
+            var lines = _textView.Text.Split('\n');
+
+            float y = 15;
+
+            float lineHeight = 20;
+
+            float leftMarginLineNumbers = 5;
+
+            float leftMarginText = 40;
+
+            canvas.FontSize = 14;
+
+            canvas.FontColor = Colors.Black;
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                // Draw line number
+                canvas.FontColor = Colors.Gray;
+                canvas.DrawString((i + 1).ToString(), leftMarginLineNumbers, y, HorizontalAlignment.Left);
+
+                // Draw text
+                canvas.FontColor = Colors.Black;
+                canvas.DrawString(lines[i], leftMarginText, y, HorizontalAlignment.Left);
+                y += lineHeight;
+            }
+
+            // TODO: Draw selection, caret, syntax highlighting, etc.
         }
     }
 }
